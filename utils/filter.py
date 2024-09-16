f5570e7549f02bca2d47e9b754461ce0a53c98ee
Lucamicheletti93 <luca.mike93@gmail.com>
2024-09-16 16:10:33 +0200

    Adding code to compute cross section

---
+++ utils/filter.py
@@ -0,0 +1,35 @@
+import sys
+import argparse
+import yaml
+import ROOT
+#sys.path.append('../utils')
+#from plot_library import LoadStyle, SetGraStat, SetGraSyst, SetLegend
+
+def main():
+    parser = argparse.ArgumentParser(description='Arguments to pass')
+    parser.add_argument('cfgFileName', metavar='text', default='config.yml', help='config file name')
+    parser.add_argument("--do_prefilter", help="Apply selections", action="store_true")
+    args = parser.parse_args()
+    
+
+    with open(args.cfgFileName, 'r') as yml_cfg:
+        inputCfg = yaml.load(yml_cfg, yaml.FullLoader)
+
+    if args.do_prefilter:
+        prefilter(inputCfg)
+
+def prefilter(config):
+    """
+    function to apply pre-filters on D0 tree according to BDT output (see config_fit.yml)
+    """
+    print("ciao")
+    fIn = ROOT.TFile(config["prefilter"]["data"], "READ")
+    fIn.ls()
+    tree = fIn.Get(config["prefilter"]["treeIn"])
+    rDataFrame = ROOT.RDataFrame(tree).Filter(config["prefilter"]["cuts"])
+    fOutName = config["prefilter"]["data"].replace(".root", config["prefilter"]["suffix"] + ".root")
+    rDataFrame.Snapshot(config["prefilter"]["treeOut"], fOutName)
+    fIn.Close()
+
+if __name__ == '__main__':
+    main()
