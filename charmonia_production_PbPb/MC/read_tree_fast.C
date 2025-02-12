e028c4e6eec094aac9a4ea5dd383ac9a7f1748a7
Lucamicheletti93 <luca.mike93@gmail.com>
2025-02-12 16:47:37 +0100

    Adding simple code to apply triggerMask

---
+++ charmonia_production_PbPb/MC/read_tree_fast.C
@@ -0,0 +1,180 @@
+#if !defined(CLING) || defined(ROOTCLING)
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <bitset>
+
+#include "TSystemDirectory.h"
+#include <TLorentzVector.h>
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2D.h"
+#include "TH1D.h"
+#include "TF1.h"
+#include "TMath.h"
+#include "TFile.h"
+#include "TString.h"
+#include "TTree.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TLatex.h"
+#include "TKey.h"
+#include "THashList.h"
+#include "TProfile.h"
+#include "TTreeReader.h"
+#include "TLine.h"
+#include "TGaxis.h"
+#include <ROOT/RDataFrame.hxx>
+
+#endif
+
+void LoadStyle();
+void SetLegend(TLegend *);
+
+const int kNsel = 51;
+
+// Taken from O2Physics/Common/CCDB/EventSelectionParams.cxx
+const char* selectionLabels[kNsel] = {
+    "kIsBBV0A", "kIsBBV0C", "kIsBBFDA", "kIsBBFDC", "kIsBBT0A", "kIsBBT0C",
+    "kNoBGV0A", "kNoBGV0C", "kNoBGFDA", "kNoBGFDC", "kNoBGT0A", "kNoBGT0C",
+    "kIsBBZNA", "kIsBBZNC", "kIsBBZAC", "kNoBGZNA", "kNoBGZNC", "kNoV0MOnVsOfPileup",
+    "kNoSPDOnVsOfPileup", "kNoV0Casymmetry", "kIsGoodTimeRange", "kNoIncompleteDAQ",
+    "kNoTPCLaserWarmUp", "kNoTPCHVdip", "kNoPileupFromSPD", "kNoV0PFPileup", "kNoSPDClsVsTklBG",
+    "kNoV0C012vsTklBG", "kNoInconsistentVtx", "kNoPileupInMultBins", "kNoPileupMV",
+    "kNoPileupTPC", "kIsTriggerTVX", "kIsINT1", "kNoITSROFrameBorder", "kNoTimeFrameBorder",
+    "kNoSameBunchPileup", "kIsGoodZvtxFT0vsPV", "kIsVertexITSTPC", "kIsVertexTOFmatched",
+    "kIsVertexTRDmatched", "kNoCollInTimeRangeNarrow", "kNoCollInTimeRangeStrict",
+    "kNoCollInTimeRangeStandard", "kNoCollInTimeRangeVzDependent", "kNoCollInRofStrict",
+    "kNoCollInRofStandard", "kNoHighMultCollInPrevRof", "kIsGoodITSLayer3",
+    "kIsGoodITSLayer0123", "kIsGoodITSLayersAll"
+};
+
+constexpr uint64_t kTriggerMask =
+    (1ULL << 37) |  // kIsGoodZvtxFT0vsPV
+    (1ULL << 32) |  // kIsTriggerTVX
+    (1ULL << 36) |  // kNoSameBunchPileup
+    (1ULL << 34) |  // kNoITSROFrameBorder
+    (1ULL << 35);   // kNoTimeFrameBorder
+
+inline bool eventSelection(uint64_t selection) {
+    return (selection & kTriggerMask) == kTriggerMask;
+}
+
+void read_tree_fast() {
+    LoadStyle();
+    uint64_t fSelection;
+    float fMass, fPt, fEta, fTauz, fTauxy, fU2Q2, fCos2DeltaPhi, fR2EP, fR2SP, fCentFT0C = -99999;
+    int fSign = -99999;
+
+    const int nMassBins = 120;
+    const double minMassRange = 2;
+    const double maxMassRange = 5;
+
+    TH2F *histMassPt = new TH2F("histMassPt", "", nMassBins, minMassRange, maxMassRange, 100, 0, 5);
+
+    string pathToFile = "/Users/lucamicheletti/Downloads/AO2D_jpsi_MC.root";
+    TFile *fIn = new TFile(pathToFile.c_str(), "READ");
+    TIter next(fIn -> GetListOfKeys()); 
+    TKey *key; 
+    while ((key = (TKey*) next())) { 
+        TString dirName = key -> GetName();
+        if (!dirName.Contains("DF_")) {
+            continue;
+        }
+
+        TTree *tree = (TTree*) fIn -> Get(Form("%s/O2rtdimuonall", dirName.Data()));
+        tree -> SetBranchAddress("fSelection", &fSelection);
+        tree -> SetBranchAddress("fMass", &fMass);
+        tree -> SetBranchAddress("fPt", &fPt);
+        tree -> SetBranchAddress("fEta", &fEta);
+        tree -> SetBranchAddress("fSign", &fSign);
+        //tree -> SetBranchAddress("fTauz", &fTauz);
+        //tree -> SetBranchAddress("fTauxy", &fTauxy);
+        //tree -> SetBranchAddress("fU2Q2", &fU2Q2);
+        //tree -> SetBranchAddress("fCos2DeltaPhi", &fCos2DeltaPhi);
+        //tree -> SetBranchAddress("fR2EP", &fR2EP);
+        //tree -> SetBranchAddress("fR2SP", &fR2SP);
+        //tree -> SetBranchAddress("fCentFT0C", &fCentFT0C);
+
+
+        for (int iEntry = 0;iEntry < tree -> GetEntries();iEntry++) {
+            tree -> GetEntry(iEntry);
+            if (!eventSelection(fSelection)) continue;
+            if (fSign == 0 && TMath::Abs(fEta) > 2.5 && TMath::Abs(fEta) < 4) {
+                if (fPt > 0 && fPt < 10) {
+                    histMassPt -> Fill(fMass, fPt);
+                }
+            }
+        }
+    }
+    fIn -> Close();
+
+    TCanvas *canvasMassPt = new TCanvas("canvasMassPt", "", 800, 600);
+    histMassPt -> Draw("COLZ");
+
+
+
+    
+}
+////////////////////////////////////////////////////////////////////////////////
+void LoadStyle(){
+    int font = 42;
+    gStyle -> SetFrameBorderMode(0);
+    gStyle -> SetFrameFillColor(0);
+    gStyle -> SetCanvasBorderMode(0);
+    gStyle -> SetPadBorderMode(0);
+    gStyle -> SetPadColor(10);
+    gStyle -> SetCanvasColor(10);
+    gStyle -> SetTitleFillColor(10);
+    gStyle -> SetTitleBorderSize(1);
+    gStyle -> SetStatColor(10);
+    gStyle -> SetStatBorderSize(1);
+    gStyle -> SetLegendBorderSize(1);
+    gStyle -> SetDrawBorder(0);
+    gStyle -> SetTextFont(font);
+    gStyle -> SetStatFontSize(0.05);
+    gStyle -> SetStatX(0.97);
+    gStyle -> SetStatY(0.98);
+    gStyle -> SetStatH(0.03);
+    gStyle -> SetStatW(0.3);
+    gStyle -> SetTickLength(0.02,"y");
+    gStyle -> SetEndErrorSize(3);
+    gStyle -> SetLabelSize(0.05,"xyz");
+    gStyle -> SetLabelFont(font,"xyz");
+    gStyle -> SetLabelOffset(0.01,"xyz");
+    gStyle -> SetTitleFont(font,"xyz");
+    gStyle -> SetTitleOffset(0.9,"x");
+    gStyle -> SetTitleOffset(1.02,"y");
+    gStyle -> SetTitleSize(0.05,"xyz");
+    gStyle -> SetMarkerSize(1.3);
+    gStyle -> SetOptStat(0);
+    gStyle -> SetEndErrorSize(0);
+    gStyle -> SetCanvasPreferGL(kTRUE);
+    gStyle -> SetHatchesSpacing(0.5);
+
+    gStyle -> SetPadLeftMargin(0.15);
+    gStyle -> SetPadBottomMargin(0.15);
+    gStyle -> SetPadTopMargin(0.05);
+    gStyle -> SetPadRightMargin(0.05);
+    gStyle -> SetEndErrorSize(0.0);
+    gStyle -> SetTitleSize(0.05,"X");
+    gStyle -> SetTitleSize(0.045,"Y");
+    gStyle -> SetLabelSize(0.045,"X");
+    gStyle -> SetLabelSize(0.045,"Y");
+    gStyle -> SetTitleOffset(1.2,"X");
+    gStyle -> SetTitleOffset(1.35,"Y");
+}
+////////////////////////////////////////////////////////////////////////////////
+void SetLegend(TLegend *legend){
+    legend -> SetBorderSize(0);
+    legend -> SetFillColor(10);
+    legend -> SetFillStyle(1);
+    legend -> SetLineStyle(0);
+    legend -> SetLineColor(0);
+    legend -> SetTextFont(42);
+    legend -> SetTextSize(0.045);
+}
