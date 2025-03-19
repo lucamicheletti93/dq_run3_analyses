9ea82833ac26cc413fb5d133aea319a581a7ea4e
Lucamicheletti93 <luca.mike93@gmail.com>
2025-03-20 00:49:41 +0100

    Update the macro to do extrapolation

--- charmonia_production_PbPb/utils/extrapolation.C
+++ charmonia_production_PbPb/utils/extrapolation.C
@@ -8,11 +8,42 @@ inline void FillPointsForCl(int nPoints, double min, double max, TF1 *func, doub
     }
 }
 
+inline int GetClosestPointIndex(TGraphErrors *graph, double x_target) {
+    int nPoints = graph -> GetN();
+    double x, y;
+    int bestIndex = -1;
+    double minDiff = 1e-2;
+
+    for (int i = 0; i < nPoints; i++) {
+        graph -> GetPoint(i, x, y);
+        double diff = fabs(x - x_target);
+        if (diff < minDiff) {
+            minDiff = diff;
+            bestIndex = i;
+        }
+    }
+    return bestIndex;
+}
+
 void extrapolation(double mySqrts = 5.36, double confLevel = 0.68) {
     LoadStyle();
 
     double extrSqrts[1] = {5.36};
-    vector<double> extrRatios, errExtrRatios;
+
+    TH1D *histRatiosErrAll = new TH1D("histRatiosErrAll", "", 3, 0, 3);
+    histRatiosErrAll -> SetLineColor(kBlack);
+    histRatiosErrAll -> SetLineWidth(2);
+    histRatiosErrAll -> SetMarkerStyle(20);
+    histRatiosErrAll -> SetMarkerColor(kBlack);
+    histRatiosErrAll -> SetFillStyle(0);
+
+    TH1D *histRatiosErrStat = new TH1D("histRatiosErrStat", "", 3, 0, 3);
+    histRatiosErrStat -> SetLineColor(kBlack);
+    histRatiosErrStat -> SetLineWidth(2);
+    histRatiosErrStat -> SetMarkerStyle(24);
+    histRatiosErrStat -> SetMarkerColor(kBlack);
+    histRatiosErrStat -> SetFillStyle(0);
+
     const int nSqrts = 4;
 
     double sqrts[] = {5.02, 7.00, 8.00, 13.00};
@@ -62,139 +93,241 @@ void extrapolation(double mySqrts = 5.36, double confLevel = 0.68) {
     graErrAllRatioVsSqrts -> SetMarkerColor(kBlack);
     graErrAllRatioVsSqrts -> SetFillStyle(0);
 
-    // Pol1
-    TF1 *funcPol1 = new TF1("funcPol1", "pol1", 2, 15);
-    funcPol1 -> SetLineColor(kRed+1);
-    graErrAllRatioVsSqrts -> Fit(funcPol1, "R0");
-
     const int nPoints = 1000;
     double xFit[nPoints], yFit[nPoints], yErr[nPoints];
-    FillPointsForCl(nPoints, 4, 14, funcPol1, xFit, yFit, yErr);
-    TGraphErrors *graFuncPol1Cl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
-    TVirtualFitter *fitterFuncPol1Cl = TVirtualFitter::GetFitter();
-    fitterFuncPol1Cl -> GetConfidenceIntervals(graFuncPol1Cl, 0.68);
-    graFuncPol1Cl -> SetFillColorAlpha(kRed+1, 0.3);
+    // Pol1
+    TF1 *funcErrAllPol1 = new TF1("funcErrAllPol1", "pol1", 2, 15);
+    funcErrAllPol1 -> SetLineStyle(kSolid);
+    funcErrAllPol1 -> SetLineColor(kRed+1);
+    graErrAllRatioVsSqrts -> Fit(funcErrAllPol1, "R0");
 
-    int indexPol1 = graFuncPol1Cl -> GetN() - 1;
-    double extrRatioPol1[1], errExtrRatioPol1[1];
-    extrRatioPol1[0] = funcPol1 -> Eval(mySqrts);
-    errExtrRatioPol1[0] = graFuncPol1Cl -> GetErrorY(indexPol1);
-    extrRatios.push_back(extrRatioPol1[0]);
-    errExtrRatios.push_back(errExtrRatioPol1[0]);
+    FillPointsForCl(nPoints, 4, 14, funcErrAllPol1, xFit, yFit, yErr);
+    TGraphErrors *graFuncErrAllPol1Cl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
+    graFuncErrAllPol1Cl -> SetName("graFuncErrAllPol1Cl");
+    TVirtualFitter *fitterFuncErrAllPol1Cl = TVirtualFitter::GetFitter();
+    fitterFuncErrAllPol1Cl -> GetConfidenceIntervals(graFuncErrAllPol1Cl, 0.68);
+    graFuncErrAllPol1Cl -> SetFillColorAlpha(kRed+1, 0.3);
 
-    TGraphErrors *graExtrRatioPol1 = new TGraphErrors(1, extrSqrts, extrRatioPol1, 0, errExtrRatioPol1);
-    graExtrRatioPol1 -> SetLineColor(kRed+1);
-    graExtrRatioPol1 -> SetLineWidth(2);
-    graExtrRatioPol1 -> SetMarkerStyle(20);
-    graExtrRatioPol1 -> SetMarkerColor(kRed+1);
-    graExtrRatioPol1 -> SetFillStyle(0);
+    TF1 *funcErrStatPol1 = new TF1("funcErrStatPol1", "pol1", 2, 15);
+    funcErrStatPol1 -> SetLineStyle(kDashed);
+    funcErrStatPol1 -> SetLineColor(kRed+1);
+    graStatRatioVsSqrts -> Fit(funcErrStatPol1, "R0");
 
+    FillPointsForCl(nPoints, 4, 14, funcErrStatPol1, xFit, yFit, yErr);
+    TGraphErrors *graFuncErrStatPol1Cl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
+    graFuncErrStatPol1Cl -> SetName("graFuncErrStatPol1Cl");
+    TVirtualFitter *fitterFuncErrStatPol1Cl = TVirtualFitter::GetFitter();
+    fitterFuncErrStatPol1Cl -> GetConfidenceIntervals(graFuncErrStatPol1Cl, 0.68);
+    graFuncErrStatPol1Cl -> SetFillStyle(3352);
+    graFuncErrStatPol1Cl -> SetFillColorAlpha(kRed+1, 0.3);
+
+    int indexPol1 = GetClosestPointIndex(graFuncErrAllPol1Cl, mySqrts);
+    double extrRatioErrAllPol1[1], errExtrRatioErrAllPol1[1];
+    extrRatioErrAllPol1[0] = funcErrAllPol1 -> Eval(mySqrts);
+    errExtrRatioErrAllPol1[0] = graFuncErrAllPol1Cl -> GetErrorY(indexPol1);
+
+    double extrRatioErrStatPol1[1], errExtrRatioErrStatPol1[1];
+    extrRatioErrStatPol1[0] = funcErrStatPol1 -> Eval(mySqrts);
+    errExtrRatioErrStatPol1[0] = graFuncErrStatPol1Cl -> GetErrorY(indexPol1);
+
+    histRatiosErrAll -> SetBinContent(1, extrRatioErrAllPol1[0]);
+    histRatiosErrAll -> SetBinError(1, errExtrRatioErrAllPol1[0]);
+
+    histRatiosErrStat -> SetBinContent(1, extrRatioErrStatPol1[0]);
+    histRatiosErrStat -> SetBinError(1, errExtrRatioErrStatPol1[0]);
+
+    TGraphErrors *graExtrRatioErrAllPol1 = new TGraphErrors(1, extrSqrts, extrRatioErrAllPol1, 0, errExtrRatioErrAllPol1);
+    graExtrRatioErrAllPol1 -> SetLineColor(kRed+1);
+    graExtrRatioErrAllPol1 -> SetLineWidth(2);
+    graExtrRatioErrAllPol1 -> SetMarkerStyle(20);
+    graExtrRatioErrAllPol1 -> SetMarkerColor(kRed+1);
+    graExtrRatioErrAllPol1 -> SetFillStyle(0);
+
+    TGraphErrors *graExtrRatioErrStatPol1 = new TGraphErrors(1, extrSqrts, extrRatioErrStatPol1, 0, errExtrRatioErrStatPol1);
+    graExtrRatioErrStatPol1 -> SetLineColor(kRed+1);
+    graExtrRatioErrStatPol1 -> SetLineWidth(2);
+    graExtrRatioErrStatPol1 -> SetMarkerStyle(24);
+    graExtrRatioErrStatPol1 -> SetMarkerColor(kRed+1);
+    graExtrRatioErrStatPol1 -> SetFillStyle(0);
+    
     // Exponential
-    TF1 *funcExp = new TF1("funcExp", "[0]*(1-exp(-[1]*x))", 2, 15);
-    funcExp -> SetLineColor(kBlue+1);
-    funcExp -> SetParameters(0.01, 1);
-    graErrAllRatioVsSqrts -> Fit(funcExp, "R0");
+    TF1 *funcErrAllExp = new TF1("funcErrAllExp", "[0]*(1-exp(-[1]*x))", 2, 15);
+    funcErrAllExp -> SetLineColor(kBlue+1);
+    funcErrAllExp -> SetParameters(0.01, 1);
+    graErrAllRatioVsSqrts -> Fit(funcErrAllExp, "R0");
 
-    FillPointsForCl(nPoints, 4, 14, funcExp, xFit, yFit, yErr);
-    TGraphErrors *graFuncExpCl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
-    TVirtualFitter *fitterFuncExpCl = TVirtualFitter::GetFitter();
-    fitterFuncExpCl -> GetConfidenceIntervals(graFuncExpCl, 0.68);
-    graFuncExpCl -> SetFillColorAlpha(kBlue+1, 0.3);
+    FillPointsForCl(nPoints, 4, 14, funcErrAllExp, xFit, yFit, yErr);
+    TGraphErrors *graFuncErrAllExpCl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
+    graFuncErrAllExpCl -> SetName("graFuncErrAllExpCl");
+    TVirtualFitter *fitterFuncErrAllExpCl = TVirtualFitter::GetFitter();
+    fitterFuncErrAllExpCl -> GetConfidenceIntervals(graFuncErrAllExpCl, 0.68);
+    graFuncErrAllExpCl -> SetFillColorAlpha(kBlue+1, 0.3);
 
-    int indexExp = graFuncExpCl -> GetN() - 1;
-    double extrRatioExp[1], errExtrRatioExp[1];
-    extrRatioExp[0] = funcExp -> Eval(mySqrts);
-    errExtrRatioExp[0] = graFuncExpCl -> GetErrorY(indexExp);
-    extrRatios.push_back(extrRatioExp[0]);
-    errExtrRatios.push_back(errExtrRatioExp[0]);
+    TF1 *funcErrStatExp = new TF1("funcErrStatExp", "pol1", 2, 15);
+    funcErrStatExp -> SetLineStyle(kDashed);
+    funcErrStatExp -> SetLineColor(kBlue+1);
+    graStatRatioVsSqrts -> Fit(funcErrStatExp, "R0");
 
-    TGraphErrors *graExtrRatioExp = new TGraphErrors(1, extrSqrts, extrRatioExp, 0, errExtrRatioExp);
-    graExtrRatioExp -> SetLineColor(kBlue+1);
-    graExtrRatioExp -> SetLineWidth(2);
-    graExtrRatioExp -> SetMarkerStyle(20);
-    graExtrRatioExp -> SetMarkerColor(kBlue+1);
-    graExtrRatioExp -> SetFillStyle(0);
+    FillPointsForCl(nPoints, 4, 14, funcErrStatExp, xFit, yFit, yErr);
+    TGraphErrors *graFuncErrStatExpCl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
+    graFuncErrStatExpCl -> SetName("graFuncErrStatExpCl");
+    TVirtualFitter *fitterFuncErrStatExpCl = TVirtualFitter::GetFitter();
+    fitterFuncErrStatExpCl -> GetConfidenceIntervals(graFuncErrStatExpCl, 0.68);
+    graFuncErrStatExpCl -> SetFillStyle(3352);
+    graFuncErrStatExpCl -> SetFillColorAlpha(kBlue+1, 0.3);
+
+    int indexExp = GetClosestPointIndex(graFuncErrAllExpCl, mySqrts);
+    double extrRatioErrAllExp[1], errExtrRatioErrAllExp[1];
+    extrRatioErrAllExp[0] = funcErrAllExp -> Eval(mySqrts);
+    errExtrRatioErrAllExp[0] = graFuncErrAllExpCl -> GetErrorY(indexExp);
+
+    double extrRatioErrStatExp[1], errExtrRatioErrStatExp[1];
+    extrRatioErrStatExp[0] = funcErrStatExp -> Eval(mySqrts);
+    errExtrRatioErrStatExp[0] = graFuncErrStatExpCl -> GetErrorY(indexExp);
+
+    histRatiosErrAll -> SetBinContent(2, extrRatioErrAllExp[0]);
+    histRatiosErrAll -> SetBinError(2, errExtrRatioErrAllExp[0]);
+
+    histRatiosErrStat -> SetBinContent(2, extrRatioErrStatExp[0]);
+    histRatiosErrStat -> SetBinError(2, errExtrRatioErrStatExp[0]);
+
+    TGraphErrors *graExtrRatioErrAllExp = new TGraphErrors(1, extrSqrts, extrRatioErrAllExp, 0, errExtrRatioErrAllExp);
+    graExtrRatioErrAllExp -> SetLineColor(kBlue+1);
+    graExtrRatioErrAllExp -> SetLineWidth(2);
+    graExtrRatioErrAllExp -> SetMarkerStyle(20);
+    graExtrRatioErrAllExp -> SetMarkerColor(kBlue+1);
+    graExtrRatioErrAllExp -> SetFillStyle(0);
+
+    TGraphErrors *graExtrRatioErrStatExp = new TGraphErrors(1, extrSqrts, extrRatioErrStatExp, 0, errExtrRatioErrStatExp);
+    graExtrRatioErrStatExp -> SetLineColor(kBlue+1);
+    graExtrRatioErrStatExp -> SetLineWidth(2);
+    graExtrRatioErrStatExp -> SetMarkerStyle(24);
+    graExtrRatioErrStatExp -> SetMarkerColor(kBlue+1);
+    graExtrRatioErrStatExp -> SetFillStyle(0);
 
     // Power Low
-    TF1 *funcPowerLow = new TF1("funcPowerLow", "[0]*x**([1])", 2, 15);
-    funcPowerLow -> SetLineColor(kGreen+1);
-    graErrAllRatioVsSqrts -> Fit(funcPowerLow, "R0");
+    TF1 *funcErrAllPowerLow = new TF1("funcErrAllPowerLow", "[0]*x**([1])", 2, 15);
+    funcErrAllPowerLow -> SetLineColor(kGreen+1);
+    graErrAllRatioVsSqrts -> Fit(funcErrAllPowerLow, "R0");
 
-    FillPointsForCl(nPoints, 4, 14, funcPowerLow, xFit, yFit, yErr);
-    TGraphErrors *graFuncPowerLowCl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
-    TVirtualFitter *fitterFuncPowerLowCl = TVirtualFitter::GetFitter();
-    fitterFuncPowerLowCl -> GetConfidenceIntervals(graFuncPowerLowCl, 0.68);
-    graFuncPowerLowCl -> SetFillColorAlpha(kGreen+1, 0.3);
+    FillPointsForCl(nPoints, 4, 14, funcErrAllPowerLow, xFit, yFit, yErr);
+    TGraphErrors *graFuncErrAllPowerLowCl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
+    graFuncErrAllPowerLowCl -> SetName("graFuncErrAllPowerLowCl");
+    TVirtualFitter *fitterFuncErrAllPowerLowCl = TVirtualFitter::GetFitter();
+    fitterFuncErrAllPowerLowCl -> GetConfidenceIntervals(graFuncErrAllPowerLowCl, 0.68);
+    graFuncErrAllPowerLowCl -> SetFillColorAlpha(kGreen+1, 0.3);
 
-    int indexPowerLow = graFuncPowerLowCl -> GetN() - 1;
-    double extrRatioPowerLow[1], errExtrRatioPowerLow[1];
-    extrRatioPowerLow[0] = funcPowerLow -> Eval(mySqrts);
-    errExtrRatioPowerLow[0] = graFuncPowerLowCl -> GetErrorY(indexPowerLow);
-    extrRatios.push_back(extrRatioPowerLow[0]);
-    errExtrRatios.push_back(errExtrRatioPowerLow[0]);
+    TF1 *funcErrStatPowerLow = new TF1("funcErrStatPowerLow", "pol1", 2, 15);
+    funcErrStatPowerLow -> SetLineStyle(kDashed);
+    funcErrStatPowerLow -> SetLineColor(kGreen+1);
+    graStatRatioVsSqrts -> Fit(funcErrStatPowerLow, "R0");
 
-    TGraphErrors *graExtrRatioPowerLow = new TGraphErrors(1, extrSqrts, extrRatioPowerLow, 0, errExtrRatioPowerLow);
-    graExtrRatioPowerLow -> SetLineColor(kGreen+1);
-    graExtrRatioPowerLow -> SetLineWidth(2);
-    graExtrRatioPowerLow -> SetMarkerStyle(20);
-    graExtrRatioPowerLow -> SetMarkerColor(kGreen+1);
-    graExtrRatioPowerLow -> SetFillStyle(0);
+    FillPointsForCl(nPoints, 4, 14, funcErrStatPowerLow, xFit, yFit, yErr);
+    TGraphErrors *graFuncErrStatPowerLowCl = new TGraphErrors(nPoints, xFit, yFit, 0, yErr);
+    graFuncErrStatPowerLowCl -> SetName("graFuncErrStatPowerLowCl");
+    TVirtualFitter *fitterFuncErrStatPowerLowCl = TVirtualFitter::GetFitter();
+    fitterFuncErrStatPowerLowCl -> GetConfidenceIntervals(graFuncErrStatPowerLowCl, 0.68);
+    graFuncErrStatPowerLowCl -> SetFillStyle(3352);
+    graFuncErrStatPowerLowCl -> SetFillColorAlpha(kGreen+1, 0.3);
+
+    int indexPowerLow = GetClosestPointIndex(graFuncErrAllPowerLowCl, mySqrts);
+    double extrRatioErrAllPowerLow[1], errExtrRatioErrAllPowerLow[1];
+    extrRatioErrAllPowerLow[0] = funcErrAllPowerLow -> Eval(mySqrts);
+    errExtrRatioErrAllPowerLow[0] = graFuncErrAllPowerLowCl -> GetErrorY(indexPowerLow);
+
+    double extrRatioErrStatPowerLow[1], errExtrRatioErrStatPowerLow[1];
+    extrRatioErrStatPowerLow[0] = funcErrStatPowerLow -> Eval(mySqrts);
+    errExtrRatioErrStatPowerLow[0] = graFuncErrStatPowerLowCl -> GetErrorY(indexPowerLow);
+
+    histRatiosErrAll -> SetBinContent(3, extrRatioErrAllPowerLow[0]);
+    histRatiosErrAll -> SetBinError(3, errExtrRatioErrAllPowerLow[0]);
+
+    histRatiosErrStat -> SetBinContent(3, extrRatioErrStatPowerLow[0]);
+    histRatiosErrStat -> SetBinError(3, errExtrRatioErrStatPowerLow[0]);
+
+    TGraphErrors *graExtrRatioErrAllPowerLow = new TGraphErrors(1, extrSqrts, extrRatioErrAllPowerLow, 0, errExtrRatioErrAllPowerLow);
+    graExtrRatioErrAllPowerLow -> SetLineColor(kGreen+1);
+    graExtrRatioErrAllPowerLow -> SetLineWidth(2);
+    graExtrRatioErrAllPowerLow -> SetMarkerStyle(20);
+    graExtrRatioErrAllPowerLow -> SetMarkerColor(kGreen+1);
+    graExtrRatioErrAllPowerLow -> SetFillStyle(0);
+
+    TGraphErrors *graExtrRatioErrStatPowerLow = new TGraphErrors(1, extrSqrts, extrRatioErrStatPowerLow, 0, errExtrRatioErrStatPowerLow);
+    graExtrRatioErrStatPowerLow -> SetLineColor(kGreen+1);
+    graExtrRatioErrStatPowerLow -> SetLineWidth(2);
+    graExtrRatioErrStatPowerLow -> SetMarkerStyle(24);
+    graExtrRatioErrStatPowerLow -> SetMarkerColor(kGreen+1);
+    graExtrRatioErrStatPowerLow -> SetFillStyle(0);
 
     // Extract the average of the extrapolated points
-    double trials[3] = {0.5, 1.5, 2.5};
-    TGraphErrors *graEstraRatios = new TGraphErrors(3, trials, &(extrRatios[0]), 0, &(errExtrRatios[0]));
-    graEstraRatios -> SetLineColor(kBlack);
-    graEstraRatios -> SetLineWidth(2);
-    graEstraRatios -> SetMarkerStyle(20);
-    graEstraRatios -> SetMarkerColor(kBlack);
-    graEstraRatios -> SetFillStyle(0);
+    TF1 *funcPol0ErrAll = new TF1("funcPol0ErrAll", "pol0", 0, 3);
+    histRatiosErrAll -> Fit(funcPol0ErrAll, "R0");
 
-    TF1 *funcPol0 = new TF1("funcPol0", "pol0", 0, 3);
-    graEstraRatios -> Fit(funcPol0, "R0");
-
-    std::cout << "Extrapolated Ratio: " << funcPol0 -> GetParameter(0) << " +/- " << funcPol0 -> GetParError(0) << std::endl;
+    TF1 *funcPol0ErrStat = new TF1("funcPol0ErrStat", "pol0", 0, 3);
+    histRatiosErrStat -> Fit(funcPol0ErrStat, "R0");
 
 
     TCanvas *canvasRatioVsSqrts = new TCanvas("canvasRatioVsSqrts", "", 1200, 1200);
     canvasRatioVsSqrts -> Divide(2, 2);
 
-    TH2D *histGridVsSqrts = new TH2D("histGridVsSqrts", "; #sqrt{#it{s}} (GeV) ; #sigma_{#psi(2S)} / #sigma_{J/#psi}", 100, 4, 14, 100, 0, 0.3);
-    TH2D *histGridVsTrials = new TH2D("histGridVsTrials", "; Trial ; #sigma_{#psi(2S)} / #sigma_{J/#psi}", 3, 0, 3, 100, 0, 0.3);
+    TH2D *histGridVsSqrts = new TH2D("histGridVsSqrts", "; #sqrt{#it{s}} (GeV) ; #sigma_{#psi(2S)} / #sigma_{J/#psi}", 100, 4, 14, 100, 0.1, 0.2);
+    TH2D *histGridVsTrials = new TH2D("histGridVsTrials", "; Trial ; #sigma_{#psi(2S)} / #sigma_{J/#psi}", 3, 0, 3, 100, 0.1, 0.2);
 
     canvasRatioVsSqrts -> cd(1);
     histGridVsSqrts -> Draw();
     graStatRatioVsSqrts -> Draw("EP SAME");
     graSystAllRatioVsSqrts -> Draw("E2 SAME");
-    funcPol1 -> Draw("SAME");
-    graFuncPol1Cl -> Draw("3 SAME");
-    graExtrRatioPol1 -> Draw("EP SAME");
+    funcErrAllPol1 -> Draw("SAME");
+    //funcErrStatPol1 -> Draw("SAME");
+    graFuncErrAllPol1Cl -> Draw("3 SAME");
+    //graFuncErrStatPol1Cl -> Draw("3 SAME");
+    graExtrRatioErrAllPol1 -> Draw("EP SAME");
+    //graExtrRatioErrStatPol1 -> Draw("EP SAME");
 
     canvasRatioVsSqrts -> cd(2);
     histGridVsSqrts -> Draw();
     graStatRatioVsSqrts -> Draw("EP SAME");
     graSystAllRatioVsSqrts -> Draw("E2 SAME");
-    funcExp -> Draw("SAME");
-    graFuncExpCl -> Draw("3 SAME");
-    graExtrRatioExp -> Draw("EP SAME");
+    funcErrAllExp -> Draw("SAME");
+    //funcErrStatExp -> Draw("SAME");
+    graFuncErrAllExpCl -> Draw("3 SAME");
+    //graFuncErrStatExpCl -> Draw("3 SAME");
+    graExtrRatioErrAllExp -> Draw("EP SAME");
+    //graExtrRatioErrStatExp -> Draw("EP SAME");
 
     canvasRatioVsSqrts -> cd(3);
     histGridVsSqrts -> Draw();
     graStatRatioVsSqrts -> Draw("EP SAME");
     graSystAllRatioVsSqrts -> Draw("E2 SAME");
-    funcPowerLow -> Draw("SAME");
-    graFuncPowerLowCl -> Draw("3 SAME");
-    graExtrRatioPowerLow -> Draw("EP SAME");
+    funcErrAllPowerLow -> Draw("SAME");
+    //funcErrStatPowerLow -> Draw("SAME");
+    graFuncErrAllPowerLowCl -> Draw("3 SAME");
+    //graFuncErrStatPowerLowCl -> Draw("3 SAME");
+    graExtrRatioErrAllPowerLow -> Draw("EP SAME");
+    //graExtrRatioErrStatPowerLow -> Draw("EP SAME");
 
     canvasRatioVsSqrts -> cd(4);
     histGridVsTrials -> Draw();
-    graEstraRatios -> Draw("EP SAME");
-    funcPol0 -> Draw("SAME");
+    funcPol0ErrAll -> Draw("SAME");
+    funcPol0ErrStat -> Draw("SAME");
+    histRatiosErrAll -> Draw("EP SAME");
+    //histRatiosErrStat -> Draw("EP SAME");
+
+    double meanErrStat = 0, meanErrAll = 0;
+    for (int iTrial = 0;iTrial < 3;iTrial++) {
+        meanErrStat += histRatiosErrStat -> GetBinError(iTrial+1);
+        meanErrAll += histRatiosErrAll -> GetBinError(iTrial+1);
+    }
+    meanErrStat = meanErrStat / 3.;
+    meanErrAll = meanErrAll / 3.;
 
     TLatex *latexTitle = new TLatex();
     latexTitle -> SetTextSize(0.050);
     latexTitle -> SetNDC();
     latexTitle -> SetTextFont(42);
-    latexTitle -> DrawLatex(0.25, 0.87, Form("#sigma_{#psi(2S)} / #sigma_{J/#psi} = %f #pm %f", funcPol0 -> GetParameter(0), funcPol0 -> GetParError(0)));
+    //latexTitle -> DrawLatex(0.2, 0.87, Form("#sigma_{#psi(2S)}/#sigma_{J/#psi} = %f #pm %f #pm %f", funcPol0ErrAll -> GetParameter(0), funcPol0ErrAll -> GetParError(0), meanErrAll));
+    //latexTitle -> DrawLatex(0.2, 0.82, Form("#sigma_{#psi(2S)}/#sigma_{J/#psi} = %f #pm %f #pm %f", funcPol0ErrStat -> GetParameter(0), funcPol0ErrStat -> GetParError(0), meanErrStat));
+    latexTitle -> DrawLatex(0.25, 0.87, Form("#sigma_{#psi(2S)}/#sigma_{J/#psi} = %f #pm %f", funcPol0ErrAll -> GetParameter(0), funcPol0ErrAll -> GetParError(0)));
 
     canvasRatioVsSqrts -> SaveAs("extrapolation.pdf");
 }
