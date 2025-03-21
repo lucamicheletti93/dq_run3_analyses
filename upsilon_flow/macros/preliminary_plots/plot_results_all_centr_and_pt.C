737ad3ce7021ddaa72aa19c4f59b503d12312ac0
Lucamicheletti93 <luca.mike93@gmail.com>
2025-03-21 17:18:05 +0100

    Adding plots of v2 vs centrality

--- upsilon_flow/macros/preliminary_plots/plot_results_all_centr_and_pt.C
+++ upsilon_flow/macros/preliminary_plots/plot_results_all_centr_and_pt.C
@@ -1,11 +1,11 @@
 void LoadStyle();
 void SetLegend(TLegend *);
 
-inline void EvalXaxis(int nVarBins, double minVarX[], double maxVarX[], double varCentr[], double varWidthStat[], double varWidthSyst[]) {
+inline void EvalXaxis(int nVarBins, double minVarX[], double maxVarX[], double varCentr[], double varWidthStat[], double varWidthSyst[], double systWidth) {
     for (int iVar = 0;iVar < nVarBins;iVar++) {
         varCentr[iVar] = (maxVarX[iVar] + minVarX[iVar]) / 2.;
         varWidthStat[iVar] = (maxVarX[iVar] - minVarX[iVar]) / 2.;
-        varWidthSyst[iVar] = 0.15;
+        varWidthSyst[iVar] = systWidth;
     }
 }
 
@@ -17,17 +17,22 @@ void plot_results_all_centr_and_pt() {
     latexTitle -> SetNDC();
     latexTitle -> SetTextFont(42);
 
-    TLine *lineZero = new TLine(-0.5, 0, 15.5, 0);
-    lineZero -> SetLineStyle(kDashed);
-    lineZero -> SetLineWidth(2);
-    lineZero -> SetLineColor(kGray+1);
+    TLine *lineZeroPt = new TLine(-0.5, 0, 15.5, 0);
+    lineZeroPt -> SetLineStyle(kDashed);
+    lineZeroPt -> SetLineWidth(2);
+    lineZeroPt -> SetLineColor(kGray+1);
 
-    // 5-50%
+    TLine *lineZeroCentr = new TLine(0, 0, 3, 0);
+    lineZeroCentr -> SetLineStyle(kDashed);
+    lineZeroCentr -> SetLineWidth(2);
+    lineZeroCentr -> SetLineColor(kGray+1);
+
+    // 5-60%
     const int nPtBins560Run3 = 3;
     double ptMin560Run3[] = {0.0, 3.0, 6.0};
     double ptMax560Run3[] = {3.0, 6.0, 15.0};
     double ptCentr560Run3[nPtBins560Run3], ptWidthStat560Run3[nPtBins560Run3], ptWidthSyst560Run3[nPtBins560Run3];
-    EvalXaxis(nPtBins560Run3, ptMin560Run3, ptMax560Run3, ptCentr560Run3, ptWidthStat560Run3, ptWidthSyst560Run3);
+    EvalXaxis(nPtBins560Run3, ptMin560Run3, ptMax560Run3, ptCentr560Run3, ptWidthStat560Run3, ptWidthSyst560Run3, 0.15);
 
     double v2Ups1sVsPt560Run3[] = {-0.009, 0.079, 0.004}; 
     double statV2Ups1sVsPt560Run3[] = {0.047, 0.052, 0.080};
@@ -46,8 +51,8 @@ void plot_results_all_centr_and_pt() {
     graSystV2Ups1sVsPt560Run3 -> SetMarkerColor(kRed+1);
     graSystV2Ups1sVsPt560Run3 -> SetFillStyle(0);
 
-    // J/psi v2 vs pT
-    // 5-50%
+    // Upsilon(1S) v2 vs pT
+    // 5-60%
     TCanvas *canvasV2Ups1sVsPtCentr560 = new TCanvas("canvasV2Ups1sVsPtCentr560", "", 800, 600);
     canvasV2Ups1sVsPtCentr560 -> SetTicks(1, 1);
 
@@ -56,7 +61,7 @@ void plot_results_all_centr_and_pt() {
     //histGridV2Ups1sVsPtCentr560 -> GetYaxis() -> SetTitle("v_{2} {EP, |#Delta#eta > 2.5|}");
     histGridV2Ups1sVsPtCentr560 -> GetYaxis() -> SetTitle("v_{2}");
     histGridV2Ups1sVsPtCentr560 -> Draw();
-    lineZero -> Draw();
+    lineZeroPt -> Draw();
     graStatV2Ups1sVsPt560Run3 -> Draw("EP SAME");
     graSystV2Ups1sVsPt560Run3 -> Draw("E2 SAME");
 
@@ -70,9 +75,9 @@ void plot_results_all_centr_and_pt() {
     latexTitle -> DrawLatex(0.18, 0.87, "ALICE Preliminary, Pb#minusPb, #sqrt{#it{s}_{NN}} = 5.36 TeV");
     latexTitle -> DrawLatex(0.18, 0.77, "#varUpsilon(1S)#rightarrow#mu^{+}#mu^{-}, 2.5 < y < 4, 5#minus60\%");
 
-    /////////////////////////////
-    // J/psi v2 vs pT vs Run 2 //
-    /////////////////////////////
+    ///////////////////////////////////
+    // Upsilon(1S) v2 vs pT vs Run 2 //
+    ///////////////////////////////////
     const int nPtBinsRun2 = 3;
     double ptCentrRun2[] = {1.8, 4.8, 10.8};
     double ptWidthStatRun2[] = {0, 0, 0};
@@ -104,13 +109,13 @@ void plot_results_all_centr_and_pt() {
     //histGridV2Ups1sVsPtCentr560Run2VsRun3 -> GetYaxis() -> SetTitle("v_{2} {EP, |#Delta#eta > 2.5|}");
     histGridV2Ups1sVsPtCentr560Run2VsRun3 -> GetYaxis() -> SetTitle("v_{2}");
     histGridV2Ups1sVsPtCentr560Run2VsRun3 -> Draw();
-    lineZero -> Draw();
+    lineZeroPt -> Draw();
     graStatV2Ups1sVsPtCentr560Run2 -> Draw("EP SAME");
     graSystV2Ups1sVsPtCentr560Run2 -> Draw("E2 SAME");
     graStatV2Ups1sVsPt560Run3 -> Draw("EP SAME");
     graSystV2Ups1sVsPt560Run3 -> Draw("E2 SAME");
 
-    TLegend *legendV2Ups1sVsPtCentr560Run2VsRun3 = new TLegend(0.20, 0.70, 0.85, 0.80, " ", "brNDC");
+    TLegend *legendV2Ups1sVsPtCentr560Run2VsRun3 = new TLegend(0.20, 0.70, 0.85, 0.85, " ", "brNDC");
     SetLegend(legendV2Ups1sVsPtCentr560Run2VsRun3);
     legendV2Ups1sVsPtCentr560Run2VsRun3 -> SetNColumns(2);
     legendV2Ups1sVsPtCentr560Run2VsRun3 -> SetTextSize(0.05);
@@ -121,8 +126,116 @@ void plot_results_all_centr_and_pt() {
     latexTitle -> DrawLatex(0.18, 0.87, "ALICE Preliminary");
     latexTitle -> DrawLatex(0.18, 0.80, "Pb#minusPb, #varUpsilon(1S)#rightarrow#mu^{+}#mu^{-}, 2.5 < y < 4, 5#minus60\%");
 
+    ///////////////////////////////////////////
+    // Upsilon(1S) v2 vs centrality vs Run 2 //
+    ///////////////////////////////////////////
+    const int nCentrBinsRun3 = 3;
+    double ptMinRun3[] = {0.0, 1.0, 2.0};
+    double ptMaxRun3[] = {1.0, 2.0, 3.0};
+    double centrCentrRun3[nCentrBinsRun3], centrWidthStatRun3[nCentrBinsRun3], centrWidthSystRun3[nCentrBinsRun3];
+    EvalXaxis(nCentrBinsRun3, ptMinRun3, ptMaxRun3, centrCentrRun3, centrWidthStatRun3, centrWidthSystRun3, 0.05);
+
+    double v2Ups1sVsCentrRun3[] = {0.031, 0.059, 0.014}; 
+    double statV2Ups1sVsCentrRun3[] = {0.038, 0.064, 0.042};
+    double systV2Ups1sVsCentrRun3[] = {0.003, 0.005, 0.009};
+
+    TGraphErrors *graStatV2Ups1sVsCentrRun3 = new TGraphErrors(nCentrBinsRun3, centrCentrRun3, v2Ups1sVsCentrRun3, centrWidthStatRun3, statV2Ups1sVsCentrRun3);
+    graStatV2Ups1sVsCentrRun3 -> SetLineColor(kRed+1);
+    graStatV2Ups1sVsCentrRun3 -> SetLineWidth(2);
+    graStatV2Ups1sVsCentrRun3 -> SetMarkerStyle(20);
+    graStatV2Ups1sVsCentrRun3 -> SetMarkerColor(kRed+1);
+
+    TGraphErrors *graSystV2Ups1sVsCentrRun3 = new TGraphErrors(nCentrBinsRun3, centrCentrRun3, v2Ups1sVsCentrRun3, centrWidthSystRun3, systV2Ups1sVsCentrRun3);
+    graSystV2Ups1sVsCentrRun3 -> SetLineColor(kRed+1);
+    graSystV2Ups1sVsCentrRun3 -> SetLineWidth(2);
+    graSystV2Ups1sVsCentrRun3 -> SetMarkerStyle(20);
+    graSystV2Ups1sVsCentrRun3 -> SetMarkerColor(kRed+1);
+    graSystV2Ups1sVsCentrRun3 -> SetFillStyle(0);
+
+    // Upsilon(1S) v2 vs centrality
+    TCanvas *canvasV2Ups1sVsCentr = new TCanvas("canvasV2Ups1sVsCentr", "", 800, 600);
+    canvasV2Ups1sVsCentr -> SetTicks(1, 1);
+
+    TH2D *histGridV2Ups1sVsCentr = new TH2D("histGridV2Ups1sVsCentr", "", 3, 0, 3, 100, -0.10, 0.3);
+    histGridV2Ups1sVsCentr -> GetXaxis() -> SetBinLabel(1, "5#minus60%");
+    histGridV2Ups1sVsCentr -> GetXaxis() -> SetBinLabel(2, "5#minus20%");
+    histGridV2Ups1sVsCentr -> GetXaxis() -> SetBinLabel(3, "20#minus60%");
+    histGridV2Ups1sVsCentr -> GetXaxis() -> SetLabelSize(0.07);
+    //histGridV2Ups1sVsCentr -> GetXaxis() -> SetTitle("#it{p}_{T} (GeV/#it{c})");
+    //histGridV2Ups1sVsCentr -> GetYaxis() -> SetTitle("v_{2} {EP, |#Delta#eta > 2.5|}");
+    histGridV2Ups1sVsCentr -> GetYaxis() -> SetTitle("v_{2}");
+    histGridV2Ups1sVsCentr -> Draw();
+    lineZeroCentr -> Draw();
+    graStatV2Ups1sVsCentrRun3 -> Draw("EP SAME");
+    graSystV2Ups1sVsCentrRun3 -> Draw("E2 SAME");
+
+    TLegend *legendV2Ups1sVsCentr = new TLegend(0.65, 0.60, 0.85, 0.80, " ", "brNDC");
+    SetLegend(legendV2Ups1sVsCentr);
+    legendV2Ups1sVsCentr -> SetTextSize(0.05);
+    legendV2Ups1sVsCentr -> AddEntry(graStatV2Ups1sVsCentrRun3, "Stat. Uncert.", "PL");
+    legendV2Ups1sVsCentr -> AddEntry(graSystV2Ups1sVsCentrRun3, "Syst. Uncert.", "F");
+    legendV2Ups1sVsCentr -> Draw();
+
+    latexTitle -> DrawLatex(0.18, 0.87, "ALICE Preliminary, Pb#minusPb, #sqrt{#it{s}_{NN}} = 5.36 TeV");
+    latexTitle -> DrawLatex(0.18, 0.77, "#varUpsilon(1S)#rightarrow#mu^{+}#mu^{-}, 2.5 < y < 4, 2 < #it{p}_{T} < 15 GeV/#it{c}");
+
+    ///////////////////////////////////
+    // Upsilon(1S) v2 vs centrality vs Run 2 //
+    ///////////////////////////////////
+    const int nCentrBinsRun2 = 3;
+    double centrCentrRun2[] = {0.65, 1.65, 2.65};
+    double centrWidthStatRun2[] = {0, 0, 0};
+    double centrWidthSystRun2[] = {0.05, 0.05, 0.05};
+
+    double v2Ups1sVsCentrRun2[] = {-0.003, 0.017, -0.030};
+    double statV2Ups1sVsCentrRun2[] = {0.030, 0.044, 0.040};
+    double systV2Ups1sVsCentrRun2[] = {0.006, 0.011, 0.005};
+
+    TGraphErrors *graStatV2Ups1sVsCentrRun2 = new TGraphErrors(nPtBinsRun2, centrCentrRun2, v2Ups1sVsCentrRun2, centrWidthStatRun2, statV2Ups1sVsCentrRun2);
+    graStatV2Ups1sVsCentrRun2 -> SetLineColor(kGray+2);
+    graStatV2Ups1sVsCentrRun2 -> SetLineWidth(2);
+    graStatV2Ups1sVsCentrRun2 -> SetMarkerStyle(20);
+    graStatV2Ups1sVsCentrRun2 -> SetMarkerColor(kGray+2);
+
+    TGraphErrors *graSystV2Ups1sVsCentrRun2 = new TGraphErrors(nPtBinsRun2, centrCentrRun2, v2Ups1sVsCentrRun2, centrWidthSystRun2, systV2Ups1sVsCentrRun2);
+    graSystV2Ups1sVsCentrRun2 -> SetLineColor(kGray+2);
+    graSystV2Ups1sVsCentrRun2 -> SetLineWidth(2);
+    graSystV2Ups1sVsCentrRun2 -> SetMarkerStyle(20);
+    graSystV2Ups1sVsCentrRun2 -> SetMarkerColor(kGray+2);
+    graSystV2Ups1sVsCentrRun2 -> SetFillStyle(0);
+
+    TCanvas *canvasV2Ups1sVsCentrRun2VsRun3 = new TCanvas("canvasV2Ups1sVsCentrRun2VsRun3", "", 800, 600);
+    canvasV2Ups1sVsCentrRun2VsRun3 -> SetTicks(1, 1);
+
+    TH2D *histGridV2Ups1sVsCentrRun2VsRun3 = new TH2D("histGridV2Ups1sVsCentrRun2VsRun3", "", 3, 0, 3, 100, -0.10, 0.3);
+    histGridV2Ups1sVsCentrRun2VsRun3 -> GetXaxis() -> SetBinLabel(1, "5#minus60%");
+    histGridV2Ups1sVsCentrRun2VsRun3 -> GetXaxis() -> SetBinLabel(2, "5#minus20%");
+    histGridV2Ups1sVsCentrRun2VsRun3 -> GetXaxis() -> SetBinLabel(3, "20#minus60%");
+    histGridV2Ups1sVsCentrRun2VsRun3 -> GetXaxis() -> SetLabelSize(0.07);
+    //histGridV2Ups1sVsCentrRun2VsRun3 -> GetYaxis() -> SetTitle("v_{2} {EP, |#Delta#eta > 2.5|}");
+    histGridV2Ups1sVsCentrRun2VsRun3 -> GetYaxis() -> SetTitle("v_{2}");
+    histGridV2Ups1sVsCentrRun2VsRun3 -> Draw();
+    lineZeroPt -> Draw();
+    graStatV2Ups1sVsCentrRun2 -> Draw("EP SAME");
+    graSystV2Ups1sVsCentrRun2 -> Draw("E2 SAME");
+    graStatV2Ups1sVsCentrRun3 -> Draw("EP SAME");
+    graSystV2Ups1sVsCentrRun3 -> Draw("E2 SAME");
+
+    TLegend *legendV2Ups1sVsCentrRun2VsRun3 = new TLegend(0.20, 0.70, 0.85, 0.85, " ", "brNDC");
+    SetLegend(legendV2Ups1sVsCentrRun2VsRun3);
+    legendV2Ups1sVsCentrRun2VsRun3 -> SetNColumns(2);
+    legendV2Ups1sVsCentrRun2VsRun3 -> SetTextSize(0.05);
+    legendV2Ups1sVsCentrRun2VsRun3 -> AddEntry(graSystV2Ups1sVsCentrRun2, "#sqrt{#it{s}_{NN}} = 5.02 TeV", "PF");
+    legendV2Ups1sVsCentrRun2VsRun3 -> AddEntry(graSystV2Ups1sVsCentrRun3, "#sqrt{#it{s}_{NN}} = 5.36 TeV", "PF"); 
+    legendV2Ups1sVsCentrRun2VsRun3 -> Draw();
+
+    latexTitle -> DrawLatex(0.18, 0.87, "ALICE Preliminary");
+    latexTitle -> DrawLatex(0.18, 0.80, "Pb#minusPb, #varUpsilon(1S)#rightarrow#mu^{+}#mu^{-}, 2.5 < y < 4, 5#minus60\%");
+
     canvasV2Ups1sVsPtCentr560 -> SaveAs("v2Ups1sVsPtCentr560.pdf");
     canvasV2Ups1sVsPtCentr560Run2VsRun3 -> SaveAs("v2Ups1sVsPtCentr560Run2VsRun3.pdf");
+    canvasV2Ups1sVsCentr -> SaveAs("v2Ups1sVsCentr.pdf");
+    canvasV2Ups1sVsCentrRun2VsRun3 -> SaveAs("v2Ups1sVsCentrRun2VsRun3.pdf");
 }
 ////////////////////////////////////////////////////////////////////////////////
 void LoadStyle(){
